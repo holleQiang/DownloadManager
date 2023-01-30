940d44979b8cf66f3f28d0da07de91c5e41e8943
zhangqiang <zhangqing.jason@bytedance.com>
2023-01-30 17:36:48 +0800

    xxx

--- library/src/main/java/com/zhangqiang/downloadmanager/support/DownloadSupport.java
+++ library/src/main/java/com/zhangqiang/downloadmanager/support/DownloadSupport.java
@@ -8,19 +8,45 @@ import java.util.List;
 
 public interface DownloadSupport {
 
+    /**
+     * load task from local
+     */
     List<DownloadTask> loadDownloadTasks();
 
+    /**
+     * if task is running before shut down，it will be auto start when reopen
+     */
     boolean isTaskRunning(DownloadTask downloadTask);
 
+    /**
+     * create download task by request
+     */
     DownloadTask createDownloadTask(DownloadRequest request);
 
+    /**
+     * create task info getter
+     */
     TaskInfo buildTaskInfo(DownloadTask downloadTask);
 
+    /**
+     * sync progress info
+     * @return notify progress update when true
+     */
     boolean handleProgressSync(DownloadTask downloadTask);
-
+    /**
+     * compute speed of task
+     * @return notify speed update when true
+     */
     boolean handleSpeedCompute(DownloadTask downloadTask);
 
+    /**
+     * if task is idle and active task size is legal，task will be start
+     * @return true is task is idle
+     */
     boolean isTaskIdle(DownloadTask downloadTask);
 
+    /**
+     * task is remove from queue
+     */
     void handleDeleteTask(DownloadTask downloadTask, boolean deleteFile);
 }
