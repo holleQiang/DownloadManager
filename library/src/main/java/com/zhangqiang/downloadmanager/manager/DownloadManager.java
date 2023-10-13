c347a708d35b2e0cc5c6bff7358899edb008b27b
zhangqiang <zhangqing.jason@bytedance.com>
2023-10-13 19:48:47 +0800

    xxx

--- library/src/main/java/com/zhangqiang/downloadmanager/manager/DownloadManager.java
+++ library/src/main/java/com/zhangqiang/downloadmanager/manager/DownloadManager.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class DownloadManager {
 
@@ -21,7 +22,7 @@ public class DownloadManager {
     private final List<DownloadPlugin> downloadPlugins = new ArrayList<>();
     private final List<DownloadTaskFactory> downloadTaskFactories = new ArrayList<>();
     private final List<DownloadTask> downloadTasks = new ArrayList<>();
-    private int activeTaskCount = 0;
+    private final AtomicInteger activeTaskCount = new AtomicInteger(0);
     private final List<OnTaskCountChangeListener> onTaskCountChangeListeners = new ArrayList<>();
     private final List<OnActiveTaskCountChangeListener> onActiveTaskCountChangeListeners = new ArrayList<>();
     private final List<OnDownloadTaskDeleteListener> onDownloadTaskDeleteListeners = new ArrayList<>();
@@ -52,18 +53,20 @@ public class DownloadManager {
     }
 
     public DownloadTask enqueue(DownloadRequest request) {
-        for (int i = 0; i < downloadTaskFactories.size(); i++) {
-            DownloadTask downloadTask = downloadTaskFactories.get(i).createTask(request);
-            if (downloadTask != null) {
-                configDownloadTask(downloadTask);
-                downloadTasks.add(downloadTask);
-                sortTasks();
-                dispatchTaskCountChange(downloadTasks.size(), downloadTasks.size() - 1);
-                downloadTask.start();
-                return downloadTask;
+        synchronized (downloadTasks) {
+            for (int i = 0; i < downloadTaskFactories.size(); i++) {
+                DownloadTask downloadTask = downloadTaskFactories.get(i).createTask(request);
+                if (downloadTask != null) {
+                    configDownloadTask(downloadTask);
+                    downloadTasks.add(downloadTask);
+                    sortTasks();
+                    dispatchTaskCountChange(downloadTasks.size(), downloadTasks.size() - 1);
+                    downloadTask.start();
+                    return downloadTask;
+                }
             }
+            throw new IllegalArgumentException("cannot find download task factory");
         }
-        throw new IllegalArgumentException("cannot find download task factory");
     }
 
     private void configDownloadTask(DownloadTask downloadTask) {
@@ -71,68 +74,78 @@ public class DownloadManager {
             @Override
             public void onStatusChange(Status newStatus, Status oldStatus) {
                 if (newStatus == Status.DOWNLOADING) {
-                    activeTaskCount++;
-                    dispatchActiveTaskCountChange(activeTaskCount, activeTaskCount - 1);
+                    int curr = activeTaskCount.incrementAndGet();
+                    dispatchActiveTaskCountChange(curr, curr - 1);
                 } else {
-                    activeTaskCount--;
-                    dispatchActiveTaskCountChange(activeTaskCount, activeTaskCount + 1);
+                    int curr = activeTaskCount.decrementAndGet();
+                    dispatchActiveTaskCountChange(curr, curr + 1);
                 }
             }
         });
     }
 
     private void sortTasks() {
-        Collections.sort(downloadTasks, new Comparator<DownloadTask>() {
-            @Override
-            public int compare(DownloadTask o1, DownloadTask o2) {
-                return Long.compare(o2.getCreateTime(), o1.getCreateTime());
-            }
-        });
+        synchronized (downloadTasks) {
+            Collections.sort(downloadTasks, new Comparator<DownloadTask>() {
+                @Override
+                public int compare(DownloadTask o1, DownloadTask o2) {
+                    return Long.compare(o2.getCreateTime(), o1.getCreateTime());
+                }
+            });
+        }
     }
 
     public synchronized void addDownloadTasks(List<? extends DownloadTask> tasks) {
-        if (tasks == null || tasks.isEmpty()) {
-            return;
+        synchronized (downloadTasks) {
+            if (tasks == null || tasks.isEmpty()) {
+                return;
+            }
+            for (DownloadTask downloadTask : tasks) {
+                configDownloadTask(downloadTask);
+            }
+            final int oldCount = getTaskCount();
+            this.downloadTasks.addAll(tasks);
+            sortTasks();
+            dispatchTaskCountChange(getTaskCount(), oldCount);
         }
-        for (DownloadTask downloadTask : tasks) {
-            configDownloadTask(downloadTask);
-        }
-        final int oldCount = getTaskCount();
-        this.downloadTasks.addAll(tasks);
-        sortTasks();
-        dispatchTaskCountChange(getTaskCount(), oldCount);
     }
 
     public void deleteTask(DownloadTask task, RemoveTaskOptions options) {
-        if (downloadTasks.remove(task)) {
-            if(task.getStatus() == Status.DOWNLOADING){
-                task.cancel();
-            }
-            if (options.isDeleteFile()) {
-                try {
-                    FileUtils.deleteFileOrThrow(new File(task.getSaveDir(), task.getSaveFileName()));
-                } catch (IOException e) {
-                    e.printStackTrace();
+        synchronized (downloadTasks) {
+            if (downloadTasks.remove(task)) {
+                if(task.getStatus() == Status.DOWNLOADING){
+                    task.cancel();
                 }
+                if (options.isDeleteFile()) {
+                    try {
+                        FileUtils.deleteFileOrThrow(new File(task.getSaveDir(), task.getSaveFileName()));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                dispatchTaskCountChange(downloadTasks.size(), downloadTasks.size() + 1);
+                dispatchDownloadTaskDelete(task);
             }
-            dispatchTaskCountChange(downloadTasks.size(), downloadTasks.size() + 1);
-            dispatchDownloadTaskDelete(task);
         }
     }
 
     public int getActiveTaskCount() {
-        return activeTaskCount;
+        return activeTaskCount.get();
     }
 
     private void dispatchTaskCountChange(int newCount, int oldCount) {
-        for (int i = onTaskCountChangeListeners.size() - 1; i >= 0; i--) {
-            onTaskCountChangeListeners.get(i).onTaskCountChange(newCount, oldCount);
+        synchronized (onTaskCountChangeListeners) {
+            for (int i = onTaskCountChangeListeners.size() - 1; i >= 0; i--) {
+                onTaskCountChangeListeners.get(i).onTaskCountChange(newCount, oldCount);
+            }
         }
     }
 
     private void dispatchActiveTaskCountChange(int newCount, int oldCount) {
-        for (int i = onActiveTaskCountChangeListeners.size() - 1; i >= 0; i--) {
-            onActiveTaskCountChangeListeners.get(i).onActiveTaskCountChange(newCount, oldCount);
+        synchronized (onActiveTaskCountChangeListeners) {
+            for (int i = onActiveTaskCountChangeListeners.size() - 1; i >= 0; i--) {
+                onActiveTaskCountChangeListeners.get(i).onActiveTaskCountChange(newCount, oldCount);
+            }
         }
     }
 
@@ -141,40 +154,58 @@ public class DownloadManager {
     }
 
     public DownloadTask getTask(int index) {
-        return downloadTasks.get(index);
+        synchronized (downloadTasks) {
+            return downloadTasks.get(index);
+        }
     }
 
     public int getTaskCount() {
-        return downloadTasks.size();
+        synchronized (downloadTasks) {
+            return downloadTasks.size();
+        }
     }
 
     public void addTaskCountChangeListener(OnTaskCountChangeListener listener) {
-        onTaskCountChangeListeners.add(listener);
+        synchronized (onTaskCountChangeListeners) {
+            onTaskCountChangeListeners.add(listener);
+        }
     }
 
     public void removeTaskCountChangeListener(OnTaskCountChangeListener listener) {
-        onTaskCountChangeListeners.remove(listener);
+        synchronized (onTaskCountChangeListeners) {
+            onTaskCountChangeListeners.remove(listener);
+        }
     }
 
     public void addActiveTaskCountChangeListener(OnActiveTaskCountChangeListener listener) {
-        onActiveTaskCountChangeListeners.add(listener);
+        synchronized (onActiveTaskCountChangeListeners) {
+            onActiveTaskCountChangeListeners.add(listener);
+        }
     }
 
     public void removeActiveTaskCountChangeListener(OnActiveTaskCountChangeListener listener) {
-        onActiveTaskCountChangeListeners.remove(listener);
+        synchronized (onActiveTaskCountChangeListeners) {
+            onActiveTaskCountChangeListeners.remove(listener);
+        }
     }
 
     public void addOnDownloadTaskDeleteListener(OnDownloadTaskDeleteListener listener){
-        onDownloadTaskDeleteListeners.add(listener);
+        synchronized (onDownloadTaskDeleteListeners) {
+            onDownloadTaskDeleteListeners.add(listener);
+        }
     }
 
     public void removeOnDownloadTaskDeleteListener(OnDownloadTaskDeleteListener listener){
-        onDownloadTaskDeleteListeners.remove(listener);
+        synchronized (onDownloadTaskDeleteListeners) {
+            onDownloadTaskDeleteListeners.remove(listener);
+        }
     }
 
     private void dispatchDownloadTaskDelete(DownloadTask downloadTask){
-        for (int i = onDownloadTaskDeleteListeners.size() - 1; i >= 0; i--) {
-            onDownloadTaskDeleteListeners.get(i).onDownloadTaskDelete(downloadTask);
+        synchronized (onDownloadTaskDeleteListeners) {
+            for (int i = onDownloadTaskDeleteListeners.size() - 1; i >= 0; i--) {
+                onDownloadTaskDeleteListeners.get(i).onDownloadTaskDelete(downloadTask);
+            }
         }
     }
 }
